98a5079134eb36a2043ec626df3e05f4b46a566c
Nathan Duncan <nathanduncan@Nathans-MacBook-Pro.local>
2021-01-04 15:12:19 +0000

    renamed public to docs

---
+++ docs/worldofwine.js
@@ -0,0 +1,22 @@
+
+  function champagne() {
+    document.getElementById("submapShow").innerHTML = "<h4>Champagne</h4><p>Champagne is perhaps the most famous wine region in the world, this is due to the sparkling white wines that bear the same name.</br></br>Colder climates and mineral rich soil produces very crisp Pinot and Chardonnay grapes.</p>"
+  };
+
+  function bordeaux() {
+    document.getElementById("submapShow").innerHTML = "<h4>Bordeaux</h4><p>Bordeaux produces some of France's best red wines. </br></br> Nestled in the south western corner of the country the long hot summers and cool sea breezes create ideal conditions for Merlot and Cabernet Sauvignon vines. </p>"
+  };
+
+  function loire() {
+    document.getElementById("submapShow").innerHTML = "<h4>The Loire Valley</h4><p>The Loire valley to the west of Paris produces some of France's top fruity and vibrant white wines. </br></br> Grapes grown here are often Chenin Blanc and Sauvignon Blanc. </p>"
+  };
+
+  function burgundy() {
+    document.getElementById("submapShow").innerHTML = "<h4>Burgundy</h4><p>Burgundy is a region in eastern France where some of the most prestigious and expensive wines are produced. </br></br> Wines from Burgundy are predominantly Chardonnay for white wines and Pinot Noir for red wines. </p>"
+  };
+
+  function rhone() {
+    document.getElementById("submapShow").innerHTML = "<h4>The Rhône Valley</h4><p>'Cotes Du Rhone' or 'hills around the Rhone river' is a valley with exceptionally good weather for wine production. </br></br> Famous for its Grenache and Syrah grape varieties and also home to the famous Chateuneuf Du pape wines. </p>"
+  };
+
+  
